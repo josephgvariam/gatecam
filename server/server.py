0e291e8841006eedbb09dfab89298b2358926cf0
joppu <josephgeorgevariam@gmail.com>
2018-08-20 23:02:53 +0530

    demo

--- server/server.py
+++ server/server.py
@@ -1,8 +1,16 @@
 from flask import Flask, request, Response, render_template, send_file
 import subprocess
+import os
 
 app = Flask(__name__, static_url_path='/static')
 app.config['PROPAGATE_EXCEPTIONS'] = True
+
+def scandirs():
+    for root, dirs, files in os.walk('/opt/caffe/demo_images'):
+        for currentFile in files:
+            exts = ('.png', '.jpg')
+            if currentFile.lower().endswith(exts):
+                os.remove(os.path.join(root, currentFile))
 
 
 @app.route('/')
@@ -12,6 +20,8 @@ def hello_world():
 
 @app.route('/processimage', methods=["POST"])
 def processimage():
+    scandirs()
+
     f = request.files['file']
 
     f.save('/opt/caffe/demo_images/demo.jpg')
