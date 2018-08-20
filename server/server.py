cccefa96475b11fa97c003c30cac6849414fc6e0
joppu <josephgeorgevariam@gmail.com>
2018-08-20 23:56:16 +0530

    demo

--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, Response, render_template, send_file
 import subprocess
 import os
+import shutil
 
 app = Flask(__name__, static_url_path='/static')
 app.config['PROPAGATE_EXCEPTIONS'] = True
@@ -8,14 +9,31 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 def scandirs():
     for root, dirs, files in os.walk('/opt/caffe/demo_images'):
         for currentFile in files:
-            exts = ('.png', '.jpg')
+            exts = ('.png', '.jpg', '.txt')
             if currentFile.lower().endswith(exts):
                 os.remove(os.path.join(root, currentFile))
 
+def copydebugimages():
+    shutil.rmtree('/opt/caffe/examples/text/static/debug')
+    files = []
+
+    for root, dirs, files in os.walk('/opt/caffe/demo_images'):
+        for currentFile in files:
+            exts = ('.jpg')
+            if currentFile.lower().endswith(exts):
+                shutil.copy(os.path.join(root, currentFile), '/opt/caffe/examples/text/static/debug')
+                files.append(os.path.join(root, currentFile))
+
+    return files
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/debug')
+def debug():
+    print(copydebugimages())
+    return render_template('debug.html')
 
 
 @app.route('/processimage', methods=["POST"])
